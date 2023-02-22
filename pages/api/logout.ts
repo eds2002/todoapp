394a6d53105342b509b11eb26491d82c7ed9a346
eds2002 <es23jr@gmail.com>
2023-02-22 15:37:50 -0500

    attempt to delete cookie using res

--- pages/api/logout.ts
+++ pages/api/logout.ts
@@ -12,9 +12,11 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   try {
-    setCookie('userID', '', { req, res, maxAge: 0, path: '/' })
-    res.status(200).json({ code: 200, message: 'SUCCESS' })
+    res
+      .setHeader('Set-Cookie', 'userID=someValue; Max-Age=0; path=/')
+      .status(200)
+      .json({ code: 200, message: 'SUCCESS' })
   } catch (e) {
-    res.status(200).json({ code: 400, message: e as string })
+    res.status(400).json({ code: 400, message: e as string })
   }
 }
